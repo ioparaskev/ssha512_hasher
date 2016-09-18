a4245b4fdf487916dfc0c83aadf7131cbddfa4fd
John Paraskevopoulos <io.paraskev@gmail.com>
2016-09-18 18:53:38 +0300

    Add README and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="SSHA512_hasher",
+    version="0.5",
+    author="John Paraskevopoulos",
+    author_email="ioparaskev@gmail.com",
+    description="Simple SSHA512 hasher with Dovecot format support",
+    license="GPL-3.0",
+    url="https://github.com/ioparaskev/ssha512_hasher",
+    packages=['an_example_pypi_project', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: GPL-3.0 License",
+    ],
+)
