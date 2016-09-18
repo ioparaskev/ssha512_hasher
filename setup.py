2bbb2ef9ef994d4331117b1d1a5999aa8f817e08
John Paraskevopoulos <io.paraskev@gmail.com>
2016-09-18 19:26:44 +0300

    setup.py fixes

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="SSHA512_hasher",
+    name="SSHA512Hasher",
     version="0.5",
     author="John Paraskevopoulos",
     author_email="ioparaskev@gmail.com",
     description="Simple SSHA512 hasher with Dovecot format support",
     license="GPL-3.0",
     url="https://github.com/ioparaskev/ssha512_hasher",
-    packages=['ssha512'],
+    packages=find_packages(),
     long_description=read('README'),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities",
-        "License :: GPL-3.0 License",
+        "License :: OSI Approved :: GNU General Public License v3 or "
+        "later (GPLv3+)",
     ],
 )
