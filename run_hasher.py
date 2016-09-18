9bcf462b1b62f0af42654b50f95cb7794fc31eca
John Paraskevopoulos <io.paraskev@gmail.com>
2016-09-18 18:53:24 +0300

    Add python2.x compatibility

--- run_hasher.py
+++ run_hasher.py
@@ -1,6 +1,11 @@
 from ssha512.dov_ssha512 import DovecotSSHA512Hasher
 from ssha512.ssha512 import SSHA512Hasher
+import sys
 
+def bi_input(text):
+    if sys.version_info[0] < 3:
+        return raw_input(text)
+    return input(text)
 
 def setup_hasher(hasher_type):
     hasher = dict(a=SSHA512Hasher, b=DovecotSSHA512Hasher)
@@ -13,18 +18,17 @@ def verify_password(word, hasher):
         input_print = '{0} with the {{SSHA512}} prefix'.format(input_print)
     input_print = '{0}:\n'.format(input_print)
 
-    cmp_hash = input(input_print)
+    cmp_hash = bi_input(input_print)
     if hasher.verify(word, cmp_hash):
         return "It's a match!"
     else:
         return "Pass doesn't match the hash!"
 
-
 if __name__ == '__main__':
-    hash_type = input('Enter [a] for simple SSHA512, [b] for dovecot '
-                      'compatibility\n')
-    choice = input('Enter [a] for encode\n[b] for decode:\n')
-    wrd = input('Enter a string:\n')
+    hash_type = bi_input('Enter [a] for simple SSHA512, [b] for dovecot'
+                         ' compatibility\n')
+    choice = bi_input('Enter [a] for encode\n[b] for decode:\n')
+    wrd = bi_input('Enter a string:\n')
     available_modes = dict(a=lambda word, hasher: hasher.encode(word),
                            b=lambda word, hasher: verify_password(word,
                                                                   sha_hasher))
